ea86ecc2ca503d1ccc42bedd5265c6019a40b0c0
Dmitry Khmelyk <delinger360@gmail.com>
2022-06-12 14:19:02 +0300

    Created UserCreadentials class which represent app user

---
+++ Auction.Data/Context/AuctionContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Auction.Data.Context
+{
+    public class AuctionContext : IdentityDbContext
+    {
+
+    }
+}
---
+++ Auction.Data/Identity/UserCredentials.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Auction.Data.Identity
+{
+    internal class UserCredentials : IdentityUser
+    {
+        public int PersonId { get; set; }
+    }
+}
