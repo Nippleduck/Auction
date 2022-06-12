bb19ddf6a8b48d17881388d432de240b25c9f894
Dmitry Khmelyk <delinger360@gmail.com>
2022-06-12 20:28:00 +0300

    Replaced LotPlacementRequest with ReviewDetails entity

---
+++ Auction.Domain/Entities/ReviewDetails.cs
@@ -0,0 +1,14 @@
+﻿using Auction.Domain.Entities.Base;
+using Auction.Domain.Entities.Enums;
+
+namespace Auction.Domain.Entities
+{
+    public class ReviewDetails : BaseEntity
+    {
+        public ReviewStatus Status { get; set; }
+        public string Feedback { get; set; }
+
+        public int LotId { get; set; }
+        public Lot Lot { get; set; }
+    }
+}
