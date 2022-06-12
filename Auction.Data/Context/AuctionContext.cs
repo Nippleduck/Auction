58c374580f0d8d8144d2992cd8051754824d8d71
Dmitry Khmelyk <delinger360@gmail.com>
2022-06-12 14:34:45 +0300

    Added db sets for domain models

--- Auction.Data/Context/AuctionContext.cs
+++ Auction.Data/Context/AuctionContext.cs
@@ -1,9 +1,19 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Auction.Data.Identity;
+using Microsoft.EntityFrameworkCore;
+using Auction.Domain.Entities;
 
 namespace Auction.Data.Context
 {
-    public class AuctionContext : IdentityDbContext
+    public class AuctionContext : DbContext
     {
+        public AuctionContext(DbContextOptions options) : base(options) { }
 
+        public DbSet<Lot> Lots { get; set; }
+        public DbSet<BiddingDetails> BiddingDetails { get; set; }
+        public DbSet<Bid> Bids { get; set; }
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+        public DbSet<Category> Categories { get; set; }
     }
 }
