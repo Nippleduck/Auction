1c747afe9f2dafc9b607fbb1d36583864d62a02b
Dmitry Khmelyk <delinger360@gmail.com>
2022-06-13 20:41:49 +0300

    created token model and declared interfaces for its services

---
+++ Auction.Authentication.JWT/Interfaces/IJwtFactory.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Auction.Authentication.JWT.Interfaces
+{
+    public interface IJwtFactory
+    {
+        Task<AccessToken> GenerateEncodedToken(string userId, string email, string role);
+        string GenerateRefreshToken(int size = 32);
+    }
+}
---
+++ Auction.Authentication.JWT/Interfaces/ITokenValidator.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Claims;
+
+namespace Auction.Authentication.JWT.Interfaces
+{
+    public interface ITokenValidator
+    {
+        ClaimsPrincipal GetPrincipalFromToken(string token, string signingKey);
+    }
+}
