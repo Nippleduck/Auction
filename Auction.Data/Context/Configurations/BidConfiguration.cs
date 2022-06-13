99280aa40c1efebc7b73e7bd5e4a21bf6fa7cba8
Dmitry Khmelyk <delinger360@gmail.com>
2022-06-13 17:31:54 +0300

    added bids table configuration

---
+++ Auction.Data/Context/Configurations/BidConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Auction.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Auction.Data.Context.Configurations
+{
+    public class BidConfiguration : IEntityTypeConfiguration<Bid>
+    {
+        public void Configure(EntityTypeBuilder<Bid> builder)
+        {
+            builder
+                .HasOne(b => b.Bidder)
+                .WithMany(p => p.Bids)
+                .HasForeignKey(b => b.BidderId);
+
+            builder
+                .Property(b => b.Price)
+                .IsRequired();
+
+            builder
+                .Property(b => b.PlacedOn)
+                .IsRequired();
+        }
+    }
+}
