897413ed8d1bdacf9e444f22ded22fc47d5776ea
Dmitry Khmelyk <delinger360@gmail.com>
2022-06-13 21:20:44 +0300

    created token validator with custom exception

---
+++ Auction.Authentication.JWT/Exceptions/TokenValidationException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Auction.Authentication.JWT.Exceptions
+{
+    [Serializable]
+    public class TokenValidationException : Exception
+    {
+        public TokenValidationException() { }
+
+        public TokenValidationException(string message) : base(message) { }
+
+        public TokenValidationException(string message, Exception innerException) : base(message, innerException) { }
+
+        protected TokenValidationException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
---
+++ Auction.Authentication.JWT/TokenValidator.cs
@@ -0,0 +1,39 @@
+﻿using Auction.Authentication.JWT.Exceptions;
+using Auction.Authentication.JWT.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Auction.Authentication.JWT
+{
+    public class TokenValidator : ITokenValidator
+    {
+        public TokenValidator() => jwtSecurityTokenHandler ??= new JwtSecurityTokenHandler();
+
+        private readonly JwtSecurityTokenHandler jwtSecurityTokenHandler;
+
+        public ClaimsPrincipal GetPrincipalFromToken(string token, string signingKey)
+        {
+            var validationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(signingKey)),
+                ValidateLifetime = false
+            };
+
+            var principal = jwtSecurityTokenHandler.ValidateToken(token, validationParameters, out var validatedToken);
+
+            if (!(validatedToken is JwtSecurityToken jwtSecurityToken) || !SignatureAlgorithmValid(jwtSecurityToken))
+                throw new TokenValidationException("Exception occured while validating token. Check token parameters");
+
+            return principal;    
+        }
+
+        private bool SignatureAlgorithmValid(JwtSecurityToken token) =>
+            token.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase);
+    }
+}
