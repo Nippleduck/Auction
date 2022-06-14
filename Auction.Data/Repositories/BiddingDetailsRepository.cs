56e49d5acfb9247210930c4961ae98c06f7957cb
Dmitry Khmelyk <delinger360@gmail.com>
2022-06-14 18:06:58 +0300

    added query methods to BiddingDetails repository

--- Auction.Data/Repositories/BiddingDetailsRepository.cs
+++ Auction.Data/Repositories/BiddingDetailsRepository.cs
@@ -1,6 +1,10 @@
 ﻿using Auction.Data.Context;
 using Auction.Data.Interfaces.Repositories;
 using Auction.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Auction.Data.Repositories
 {
@@ -8,5 +12,17 @@ namespace Auction.Data.Repositories
     {
         public BiddingDetailsRepository(AuctionContext context) : base(context) { }
 
+        public async Task<BiddingDetails> GetByIdWithDetailsAsync(int id, CancellationToken ct) =>
+            await context.BiddingDetails
+                .Include(bd => bd.Bids)
+                .ThenInclude(bid => bid.Bidder)
+                .FirstOrDefaultAsync(bid => bid.Id == id, ct);
+
+        public async Task<IEnumerable<BiddingDetails>> GetAllWithDetailsAsync(CancellationToken ct) =>
+            await context.BiddingDetails
+                .AsNoTracking()
+                .Include(bd => bd.Bids)
+                .ThenInclude(bid => bid.Bidder)
+                .ToListAsync(ct);
     }
 }
