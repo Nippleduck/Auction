36cc027d35a6687b507796ae68e291ef12cff4d5
Dmitry Khmelyk <delinger360@gmail.com>
2022-06-14 11:45:56 +0300

    Created abstract repository class with corresponding interface

---
+++ Auction.Data/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using Auction.Domain.Entities.Base;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Auction.Data.Interfaces.Repositories
+{
+    public interface IRepository<TEntity, TId> where TEntity : BaseEntity<TId>
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken ct = default);
+        Task<TEntity> GetByIdAsync(TId id, CancellationToken ct = default);
+        Task AddAsync(TEntity entity, CancellationToken ct = default);
+        Task DeleteByIdAsync(TId id, CancellationToken ct = default);
+        void Delete(TEntity entity);
+        void Update(TEntity entity);
+    }
+}
---
+++ Auction.Data/Repositories/BaseRepository.cs
@@ -0,0 +1,38 @@
+﻿using Auction.Data.Interfaces.Repositories;
+using Auction.Domain.Entities.Base;
+using Auction.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Auction.Data.Repositories
+{
+    public abstract class BaseRepository<TEntity> : IRepository<TEntity, int>
+        where TEntity : BaseEntity 
+    {
+        protected BaseRepository(AuctionContext context) => this.context = context;
+
+        protected readonly AuctionContext context;
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken ct = default) =>
+            await context.Set<TEntity>().ToListAsync(ct);
+
+        public async Task<TEntity> GetByIdAsync(int id, CancellationToken ct = default) =>
+            await context.Set<TEntity>().FirstOrDefaultAsync(e => e.Id == id, ct);
+
+        public async Task AddAsync(TEntity entity, CancellationToken ct = default) =>
+            await context.Set<TEntity>().AddAsync(entity, ct);
+
+        public async Task DeleteByIdAsync(int id, CancellationToken ct = default)
+        {
+            var entity = await context.Set<TEntity>().FirstOrDefaultAsync(entity => entity.Id == id, ct);
+            
+            context.Set<TEntity>().Remove(entity);
+        }
+
+        public void Delete(TEntity entity) => context.Set<TEntity>().Remove(entity);
+
+        public void Update(TEntity entity) => context.Entry(entity).State = EntityState.Modified;
+    }
+}
