f127c6cff5cf9a498b4077d7b5d1668bbdcb6495
Dmitry Khmelyk <delinger360@gmail.com>
2022-06-14 18:46:16 +0300

    added default properety value for status property

---
+++ Auction.Data/Context/Configurations/ReviewConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Auction.Domain.Entities;
+using Auction.Domain.Entities.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Auction.Data.Context.Configurations
+{
+    public class ReviewConfiguration : IEntityTypeConfiguration<ReviewDetails>
+    {
+        public void Configure(EntityTypeBuilder<ReviewDetails> builder)
+        {
+            builder.Property(r => r.Status)
+                .HasDefaultValue(ReviewStatus.PendingReview);
+        }
+    }
+}
