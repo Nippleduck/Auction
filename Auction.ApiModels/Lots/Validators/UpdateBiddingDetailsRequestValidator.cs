1d1cb8591cc9bdeea0a6c9ca8b95434a0c1a410a
Dmitry Khmelyk <delinger360@gmail.com>
2022-07-07 11:44:02 +0300

    added details update request validators

---
+++ Auction.ApiModels/Lots/Validators/UpdateBiddingDetailsRequestValidator.cs
@@ -0,0 +1,13 @@
+﻿using Auction.ApiModels.Lots.Requests;
+using FluentValidation;
+
+namespace Auction.ApiModels.Lots.Validators
+{
+    public class UpdateBiddingDetailsRequestValidator : AbstractValidator<UpdateBiddingDetailsRequest>
+    {
+        public UpdateBiddingDetailsRequestValidator()
+        {
+            RuleFor(x => x.LotId).NotEmpty();
+        }
+    }
+}
